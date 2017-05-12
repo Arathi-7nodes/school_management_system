65d55229f554d1228412f9f69a00f20d40a2ff41
shibina <shibina@7nodes.com>
2017-05-12 10:21:23 +0530

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :users
+  resource :students
   root "users#wel"
   get "/wel" => "users#wel"
    get "/login" => "users#login"
+   get "new" => "students#new"
+   get "/form" => "students#form"
+  get "/listing" => "students#listing"
+  get "/details" => "students#details"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
